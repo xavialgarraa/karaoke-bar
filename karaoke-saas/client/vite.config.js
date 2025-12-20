137424ca4d4e5f8806ce717db16197b16ac7af58
xavialgarraa <131766832+xavialgarraa@users.noreply.github.com>
2025-12-20 19:35:38 +0100

    json

--- karaoke-saas/client/vite.config.js
+++ karaoke-saas/client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/karaoke-bar/', // <- añade esto, con el nombre de tu repo
 })
